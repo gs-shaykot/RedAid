df0800672ac5cba7e288ba98acd4595ad1183a6a
GS SHAYKOT <shaykotkhan1916@gmail.com>
2025-01-14 02:09:19 +0600

    how it works added

--- src/Layout/HomeLayout.jsx
+++ src/Layout/HomeLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Components/Banner';
 import WTDonate from '../Components/WTDonate';
+import How from '../Components/How';
 
 const HomeLayout = () => {
     return (
         <div>
             <Banner></Banner>
             <WTDonate></WTDonate>
+            <How></How>
         </div>
     );
 };
