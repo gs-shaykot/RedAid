30016b00252032f5f17671a9548f16cbca7c635e
GS SHAYKOT <shaykotkhan1916@gmail.com>
2025-01-14 14:28:47 +0600

    footer added

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'; 
 import { Outlet } from 'react-router-dom'; 
 import NavBar from '../Components/NavBar'; 
+import Footer from './../Components/Footer';
 
 const MainLayout = () => {
     return (
         <div className='font-roboto'>
             <NavBar></NavBar>
             <Outlet></Outlet> 
+            <Footer></Footer>
         </div>
     );
 };
